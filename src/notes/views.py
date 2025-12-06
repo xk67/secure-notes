63e1dda0a27c91c843e0c4d01837485a9ee90f5c
xk67 <xk67@tuta.io>
2025-12-06 14:11:56 +0100

    feat(notes): implement note creation view logic

--- src/notes/views.py
+++ src/notes/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Note
+from .forms import NoteForm
 
 def index(request):
-    return render(request, "notes/index.html")
+    if request.method == "POST":
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print(form.data)
+            return redirect("notes:index")
+    else:
+        form = NoteForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "notes/index.html", context)
