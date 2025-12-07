413470502f8a9b65971f79ee2c4a04024ac74134
xk67 <xk67@tuta.io>
2025-12-07 14:26:44 +0100

    feat(notes): require login for notes index view

--- src/notes/views.py
+++ src/notes/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Note
 from .forms import NoteForm
 import markdown
 
+@login_required
 def index(request):
     if request.method == "POST":
         form = NoteForm(request.POST)
