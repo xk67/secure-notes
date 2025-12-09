4807212b52c587b21af9cdbe03ace6dddaeb7d88
xk67 <xk67@tuta.io>
2025-12-09 11:20:15 +0100

    feat(users): create custom signup form with email field

--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from .forms import SignUpForm
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy("login")
