5ca5a16e9eed4671754aa18ab55699559a950c20
xk67 <xk67@tuta.io>
2025-12-10 14:52:41 +0100

    feat(users): add uid and token generation for email confirmation

--- src/users/views.py
+++ src/users/views.py
@@ -3,19 +3,26 @@ from django.shortcuts import render
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.sites.shortcuts import get_current_site
 
 def signup(request):
     if request.method == "POST":
        form = SignUpForm(request.POST)
        if form.is_valid():
-            form.save()
+            user = form.save()
 
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
+            username = user.username
+            email = user.email
+            domain = get_current_site(request).domain
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            token = default_token_generator.make_token(user)
 
             html_content = render_to_string(
                 "users/verification.html",
-                context={"username": username},
+                context={"username": username, "domain": domain,"uid": uid, "token": token},
             )
             text_content = strip_tags(html_content)
 
