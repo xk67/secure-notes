4d9512f2c0b44afa1f8a2edd2eeb8829cbe8b3e7
xk67 <xk67@tuta.io>
2025-12-10 16:36:24 +0100

    feat(users): implement verify view that handles user activation

--- src/users/views.py
+++ src/users/views.py
@@ -4,9 +4,13 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.contrib.auth.tokens import default_token_generator
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth import get_user_model
+from django.http import Http404
+
+User = get_user_model()
 
 def signup(request):
     if request.method == "POST":
@@ -39,3 +43,21 @@ def signup(request):
         form = SignUpForm
     
     return render(request, 'users/signup.html', {'form': form})
+
+def verify(request, uidb64, token):
+
+    # Use a try block because handle user input, base64 decoding or missing users can cause errors
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+        print(user.username)
+    except:
+        user = None
+    
+    if user and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return render(request, "users/verification_confirm.html")
+
+    # Return 404 for invalid tokens or non-existent users
+    raise Http404()
