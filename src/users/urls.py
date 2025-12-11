dfc5478b2fccd9e410acdd9bdcbc47f55e81dd48
xk67 <xk67@tuta.io>
2025-12-11 08:08:04 +0100

    feat(users): implement views related to password reset functionality

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from . import views
 
 urlpatterns = [
     path("login", LoginView.as_view(template_name="users/login.html", redirect_authenticated_user=True), name="login"),
     path("signup", views.signup, name="signup"),
-    path("verify/<uidb64>/<token>", views.verify, name="verify")
+    path("verify/<uidb64>/<token>", views.verify, name="verify"),
+    path("password_reset", PasswordResetView.as_view(), name="password_reset"),
+    path("password_reset/done/", PasswordResetDoneView.as_view(), name="password_reset_done"),  
+    path("reset/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset/done/", PasswordResetCompleteView.as_view(), name="password_reset_complete")
 ]
