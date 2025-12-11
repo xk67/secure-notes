4dd4066928feb0dc87ab4530e532c1d7f6500d64
xk67 <xk67@tuta.io>
2025-12-11 08:29:43 +0100

    feat(users): add custom template for PasswordResetView

--- src/users/urls.py
+++ src/users/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("login", LoginView.as_view(template_name="users/login.html", redirect_authenticated_user=True), name="login"),
     path("signup", views.signup, name="signup"),
     path("verify/<uidb64>/<token>", views.verify, name="verify"),
-    path("password_reset", PasswordResetView.as_view(from_email="noreply@secure-notes.de"), name="password_reset"),
+    path("password_reset", PasswordResetView.as_view(from_email="noreply@secure-notes.de", template_name="users/password_reset_form.html"), name="password_reset"),
     path("password_reset/done/", PasswordResetDoneView.as_view(), name="password_reset_done"),  
     path("reset/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("reset/done/", PasswordResetCompleteView.as_view(), name="password_reset_complete")
