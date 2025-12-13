44df41ddc8ab276d183d76812aa53749e4ad66b7
xk67 <xk67@tuta.io>
2025-12-14 00:07:54 +0100

    feat(notes): add owner attribute to note model

--- src/notes/models.py
+++ src/notes/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.conf import settings
 import uuid
 
 class Note(models.Model):
-    #owner = None
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="notes"
+    )
     title = models.CharField(max_length=32)
     content = models.TextField()
     private = models.BooleanField(default=True)
