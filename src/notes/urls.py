cbafc7093a27194840e0da337a82c6478e20910c
xk67 <xk67@tuta.io>
2025-12-14 01:09:58 +0100

    feat(notes): register notes/<uuid>/ endpoint in URLs

--- src/notes/urls.py
+++ src/notes/urls.py
@@ -6,5 +6,6 @@ app_name = "notes"
 urlpatterns = [
     path("", views.note, name="index"),
     path("notes/create", views.create_note, name="create_note"),
-    path("notes", views.list_notes, name="list_notes")
+    path("notes", views.list_notes, name="list_notes"),
+    path("notes/<uuid>", views.list_notes, name="show_note")
 ]
