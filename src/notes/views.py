6fe897493b01e4d400c6c1f392d5b1c2baed450b
xk67 <xk67@tuta.io>
2025-12-16 11:51:08 +0100

    feat(notes): get note content using UUID

--- src/notes/views.py
+++ src/notes/views.py
@@ -3,10 +3,12 @@ from django.contrib.auth.decorators import login_required
 from .models import Note
 from .forms import NoteForm
 from django.http import HttpResponse, Http404
-from .serializers import NoteSerializer
-from rest_framework.decorators import api_view
+from .serializers import NoteSerializer, NoteContentSerializer
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 import markdown
+from uuid import UUID
+from rest_framework.generics import get_object_or_404
 
 @login_required
 def note(request):
@@ -59,9 +61,26 @@ def show_note(request, uuid):
     return HttpResponse(html) 
 
 @api_view(['GET'])
+#@permission_classes([IsAuthenticated])
 def api_list_notes(request):
 
     if request.method == "GET":
         notes = Note.objects.all()
         serializer = NoteSerializer(notes, many=True)
         return Response(serializer.data)
+
+@api_view(['GET'])
+def api_get_note(request, uuid):
+    
+    try:
+        UUID(uuid, version=4)
+    except ValueError:
+        raise Http404() 
+
+    try:
+        note = Note.objects.get(uuid=uuid)
+    except Note.DoesNotExist:
+        raise Http404("Not found.")
+
+    serializer = NoteContentSerializer(note)
+    return Response(serializer.data)
