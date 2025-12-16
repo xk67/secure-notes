760aed25df5c32ddf4985f71ca92893cb4995067
xk67 <xk67@tuta.io>
2025-12-16 19:25:28 +0100

    feat(api): add endpoint to obtain API token

--- src/notes/urls.py
+++ src/notes/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 app_name = "notes"
@@ -9,5 +10,6 @@ urlpatterns = [
     path("notes", views.list_notes, name="list_notes"),
     path("notes/<uuid>", views.show_note, name="show_note"),
     path("api/notes", views.api_list_notes),
-    path("api/note/<uuid>", views.api_get_note)
+    path("api/note/<uuid>", views.api_get_note),
+    path('api/token', obtain_auth_token)
 ]
