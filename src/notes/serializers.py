a52b670af706635b1a1c37fdbcee8fea17a83b0c
xk67 <xk67@tuta.io>
2025-12-17 15:01:51 +0100

    feat(api): add NoteCreateSerializer class

--- src/notes/serializers.py
+++ src/notes/serializers.py
@@ -15,3 +15,12 @@ class NoteContentSerializer(serializers.ModelSerializer):
         model = Note
         fields = ["content"]
         read_only_fields = ["content"]
+
+class NoteCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["title", "content", "private"]
+
+    def create(self, validated_data):
+        user = self.context["user"]
+        return Note.objects.create(owner=user, **validated_data)
