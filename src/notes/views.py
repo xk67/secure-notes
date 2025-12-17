1575ffaf7ce274eefdf4202454f455d878b29cce
xk67 <xk67@tuta.io>
2025-12-17 15:04:32 +0100

    feat(api): implement api_create_note view

--- src/notes/views.py
+++ src/notes/views.py
@@ -3,11 +3,12 @@ from django.contrib.auth.decorators import login_required
 from .models import Note
 from .forms import NoteForm
 from django.http import HttpResponse, Http404
-from .serializers import NoteSerializer, NoteContentSerializer
+from .serializers import NoteSerializer, NoteContentSerializer, NoteCreateSerializer
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
+from rest_framework import status
 import markdown
 from uuid import UUID
 
@@ -91,3 +92,17 @@ def api_get_note(request, uuid):
 
     serializer = NoteContentSerializer(note)
     return Response(serializer.data)
+
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def api_create_note(request):
+
+    serializer = NoteCreateSerializer(data=request.data, context={"user": request.user})
+
+    if serializer.is_valid():
+        note = serializer.save()
+        return Response({"uuid": note.uuid}, status=status.HTTP_201_CREATED
+        )
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
