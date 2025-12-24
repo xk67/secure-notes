4440b741e0b612e8ee9f8c19caf418fa6ab72348
xk67 <xk67@tuta.io>
2025-12-24 14:38:32 +0100

    feat(notes): add markdown2html_safe util with bleach sanitization

--- src/notes/utils.py
+++ src/notes/utils.py
@@ -1,4 +1,30 @@
 import markdown
+import bleach
+#from django.utils.safestring import mark_safe
 
-def markdown2html_safe(mardown: str):
-    return markdown.markdown(mardown)
+ALLOWED_TAGS = [
+    'p', 'hr', #'br'
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+    'blockquote',
+    'ul', 'ol', 'li',
+    'pre', 'code',
+    'a',
+    'em', 'strong',
+    'img'
+]
+
+ALLOWED_ATTRIBUTES = {
+    'a': ['href', 'title'],
+    'img': ['src', 'alt']
+}
+
+#ALLOWED_PROTOCOLS: http, https, mailto
+
+def markdown2html_safe(content: str):
+
+    html_unsafe = markdown.markdown(content)
+    # set strip to true?
+    html_safe = bleach.clean(html_unsafe, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES,strip=True)
+    print(html_safe)
+
+    return html_safe
