47987d7e1b32651f6024378b9272be848856a038
xk67 <xk67@tuta.io>
2025-12-28 16:28:00 +0100

    feat(users): register profile view in urls

--- src/users/urls.py
+++ src/users/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path("signup", views.signup, name="signup"),
     path("verify/<uidb64>/<token>", views.verify, name="verify"),
     path("password_reset", PasswordResetView.as_view(from_email="noreply@secure-notes.de", template_name="users/password_reset_form.html"), name="password_reset"),
-    path("password_reset/done/", PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name="password_reset_done"),  
+    path("password_reset/done/", PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name="password_reset_done"),
     path("reset/<uidb64>/<token>/", PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
     path("reset/done/", PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name="password_reset_complete"),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
