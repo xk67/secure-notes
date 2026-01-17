7eba4c1565642ee8fb24009debadde319b4cc254
xk67 <xk67@tuta.io>
2026-01-17 15:46:34 +0100

    feat(utils): only allow iframes with youtube link

--- src/notes/utils.py
+++ src/notes/utils.py
@@ -21,10 +21,15 @@ ALLOWED_TAGS = [
     'div', 'button', 'template'
 ]
 
+def iframe_attribute_filter(tag, name, value):
+    if name == 'src':
+        return value.startswith('https://www.youtube-nocookie.com/embed/')
+    return name in ['title', 'referrerpolicy', 'frameborder', 'allowfullscreen']
+
 ALLOWED_ATTRIBUTES = {
     'a': ['href', 'title'],
     'img': ['src', 'alt', 'title'],
-    'iframe': ['src', 'title', 'allow', 'referrerpolicy', 'frameborder'],
+    'iframe': iframe_attribute_filter,
     'div': ['class']
 }
 
